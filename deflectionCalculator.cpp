1e25caa184a393677fbcfb7bc2512641f98d23fe
Joel Wasserman <joel.wasserman8@gmail.com>
2014-09-12 15:00:33 -0400

    bug fix for printing out top row in for loop

--- deflectionCalculator.cpp
+++ deflectionCalculator.cpp
@@ -10,32 +10,32 @@ float length, weight, base, height;
 const char seperator = ' ';
 const int numWidth = 12;
 float answers[] = {1.49e9, .187e9, 3.9e9};
-String materials[] = {Aluminum, Wood, Steel};
+string materials[] = {"Aluminum", "Wood", "Steel"};
 int i = 0;
 
 int main()
 {
 
-  cout << "Enter the Length: ";
+  cout << "Enter the Length (ft): ";
   cin >> length;
-  cout << "Weight: ";
+  cout << "Weight (lb): ";
   cin >> weight;
-  cout << "Base: ";
+  cout << "Base (ft): ";
   cin >> base;
-  cout << "Height: ";
+  cout << "Height (ft): ";
   cin >> height;
 
+  cout << left << setw(numWidth) << "material";
+  cout << left << setw(numWidth) << "elasticity";
+  cout << left << setw(numWidth) << "length(ft)";
+  cout << left << setw(numWidth) << "weight(lb)";
+  cout << left << setw(numWidth) << "base(ft)";
+  cout << left << setw(numWidth) << "height(ft)";
+  cout << left << setw(numWidth) << "max deflection" << endl;
   for (i = 0; i < 3; i++) {
     answers[i] = ((4 * ((length * length * length) * weight)) / ((answers[i] * base) * (height * height * height)));
     cout.precision(2);
-    cout << "The answer is: " << answers[i] << endl;
-    cout << left << setw(numWidth) << "material";
-    cout << left << setw(numWidth) << "elasticity";
-    cout << left << setw(numWidth) << "length(ft)";
-    cout << left << setw(numWidth) << "weight(lb)";
-    cout << left << setw(numWidth) << "base(ft)";
-    cout << left << setw(numWidth) << "height(ft)";
-    cout << left << setw(numWidth) << "max deflection" << endl;
+
     cout << setw(numWidth) << materials[i];
     cout << setw(numWidth) << EL_steel;
     cout << setw(numWidth) << length;
